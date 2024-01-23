735d60d438878e1c0611d9384d36e5768e75f75a
Juliano Eiki Hiroi Tesser <juca.sup@gmail.com>
2024-01-23 11:10:47 -0300

    Add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,72 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import LogoImage from '../assets/logo-no-background.svg'
+import { Montserrat } from 'next/font/google'
+import { FaGithub } from 'react-icons/fa6'
+import { FaInstagram } from 'react-icons/fa'
+const montserrat = Montserrat({ weight: '400', subsets: ['latin'] })
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex h-screen flex-col">
+      <nav className="flex  h-24 w-full items-center justify-between bg-teal-500">
+        <div className="ml-10 flex items-center justify-center">
+          <Image
+            src={LogoImage}
+            width={150}
+            height={150}
+            alt="Picture of the logo"
+          />
+        </div>
+        <div className="mr-10 flex justify-end space-x-5">
+          <Link href={'./login'}>Login</Link>
+          <Link href={'./register'}>Register</Link>
+        </div>
+      </nav>
+      <div>
+        <h1 className="mb-4  mt-10 flex w-full items-center justify-center text-3xl font-bold">
+          Bem vindo ao UserSystem
+        </h1>
+        <div className="flex items-center justify-center">
+          <p
+            className={
+              'w-4/6 bg-gray-100 p-8   text-justify indent-5 text-xl shadow-md ' +
+              montserrat.className
+            }
+          >
+            O UserSystem é um sistema de login e registro de usuários
+            desenvolvido com NextJS, TailwindCSS e Typescript. Projetado para
+            abranger todo o processo de autenticação, desde o registro até o
+            login, este sistema tem como propósito principal facilitar o
+            aprendizado de novas tecnologias e compreender o fluxo de
+            autenticação de usuários. Composto por três páginas principais -
+            login, register e dashboard - oferece uma experiência intuitiva. Na
+            página de login, os usuários inserem suas credenciais para acessar o
+            sistema; na página de registro, criam suas contas; e na página de
+            dashboard, após o login, visualizam projetos e realizam logout
+            conforme necessário.
+          </p>
+        </div>
+      </div>
+      <footer className="absolute bottom-0 flex h-28 w-full items-center justify-center space-x-32 bg-teal-500">
+        <Link
+          href="https://github.com/JulianoHiroi"
+          target="_blank"
+          className="text-md flex items-center text-teal-900"
+        >
+          <FaInstagram />
+          JulianoHiroi
+        </Link>
+        <p className="text-md text-teal-900">&copy;2024 JulianoHiroi</p>
+        <Link
+          href="https://www.instagram.com/julianohiroi/"
+          target="_blank"
+          className="text-md flex items-center text-teal-900"
+        >
+          <FaGithub />
+          JulianoHiroi
+        </Link>
+      </footer>
     </div>
   )
 }
