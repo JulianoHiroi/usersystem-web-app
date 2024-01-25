9119b0e096ce2ee034e705ea2be482af8c350cfb
Juliano Eiki Hiroi Tesser <juca.sup@gmail.com>
2024-01-24 22:40:54 -0300

    bump api

--- services/api.ts
+++ services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   // baseURL: 'http://localhost:4000/api/',
-  baseURL: 'http://138.197.114.241/api/',
+  baseURL: 'https://www.usersystem.codes/api',
 })
 
 export default api
